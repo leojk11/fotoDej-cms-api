2239484b5b5cda91efcf0bfc83464a5151ecedc3
Leonid Jakimovski <ljakimovski211@yahoo.com>
2022-08-25 23:41:36 +0200

    Small changes

--- helpers/errorMessages.js
+++ helpers/errorMessages.js
@@ -5,6 +5,6 @@ exports.errorMessages = {
     required_field: (field) => `${ field } is required.`,
     please_enter: (field) => `Plase enter your ${ field }.`,
     not_exist: (cluster, id) => `${ cluster } with ID ${ id } does not exist!`,
-    user_not_exist: (email) => `User with ${ email } does not exist.`,
+    user_not_exist: (email) => `User with email ${ email } does not exist.`,
     invalid_id: (id) => `Invalid ID [${ id }].`
 };
